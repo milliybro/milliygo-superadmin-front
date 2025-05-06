2a601eea0351253bbffc142ccfb15b5e8fdde8bf
G'ulomov Alisher <a.g'ulomov@uzinfocom.uz>
2025-05-06 16:37:49 +0500

    feat: add actions

--- src/features/accommodation-facilities/components/user-action-button.tsx
+++ src/features/accommodation-facilities/components/user-action-button.tsx
@@ -1,6 +1,6 @@
 import { Button } from 'antd'
 import { useTranslation } from 'react-i18next'
-import { useLocation, useNavigate } from 'react-router'
+import { useLocation, useNavigate, useSearchParams } from 'react-router'
 
 import useUserModalStore from '../store/user-modal-store'
 
@@ -8,8 +8,8 @@ import useUserModalStore from '../store/user-modal-store'
 import DeleteIcon from '@/components/icons/delete'
 
 import { useState } from 'react'
-import { useMutation } from '@tanstack/react-query'
-import { deleteUser } from '../api'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { confirmAccommodation, deleteUser } from '../api'
 import ConfirmationModal from '@/components/ui/confirmation-modal'
 import EyeIcon from '@/components/icons/eye'
 
@@ -17,11 +17,22 @@ import EyeIcon from '@/components/icons/eye'
 //   id?: any
 // }
 
-const UserActionButton = ({ id, refetch }: { id: number; refetch: any }) => {
+const UserActionButton = ({
+  id,
+  slug,
+  refetch,
+}: {
+  id: number
+  slug: string
+  refetch: any
+}) => {
   const navigate = useNavigate()
   const { t } = useTranslation()
   const { pathname } = useLocation()
   const [deleteModal, setDeleteModal] = useState(false)
+  const [searchParams] = useSearchParams()
+  const activeTab = searchParams.get('tab')
+  const queryClient = useQueryClient()
 
   const { openModal } = useUserModalStore(store => store)
 
@@ -34,6 +45,32 @@ const UserActionButton = ({ id, refetch }: { id: number; refetch: any }) => {
     onSuccess: () => {
       setDeleteModal(false)
       refetch()
+    },
+  })
+
+  const confirm = useMutation({
+    mutationFn: () => confirmAccommodation(slug, { status: true }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['hotels-data'] })
+
+      // notification.success({
+      //   message: editUserId
+      //     ? t('fields.user-notification.edit.message')
+      //     : t('fields.user-notification.add.message'),
+      // })
+    },
+  })
+
+  const reject = useMutation({
+    mutationFn: () => confirmAccommodation(slug, { status: false }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['hotels-data'] })
+
+      // notification.success({
+      //   message: editUserId
+      //     ? t('fields.user-notification.edit.message')
+      //     : t('fields.user-notification.add.message'),
+      // })
     },
   })
 
@@ -55,6 +92,27 @@ const UserActionButton = ({ id, refetch }: { id: number; refetch: any }) => {
       >
         <DeleteIcon className="text-[20px]" /> {t('common.delete')}
       </Button> */}
+      {activeTab === '2' ? (
+        <>
+          <Button
+            type="link"
+            className="text-[16px] font-medium px-0"
+            onClick={() => confirm.mutate()}
+            loading={confirm.isPending}
+          >
+            {t('common.confirm')}
+          </Button>
+          <Button
+            type="link"
+            danger
+            className="text-[16px] font-medium px-0"
+            onClick={() => reject.mutate()}
+            loading={reject.isPending}
+          >
+            {t('common.reject')}
+          </Button>
+        </>
+      ) : null}
       <ConfirmationModal
         danger
         icon={DeleteIcon}
