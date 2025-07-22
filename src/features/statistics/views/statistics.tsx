f27cff43095db5ef6f6c33a0535e19e44cf9bcb2
G'ulomov Alisher <a.g'ulomov@uzinfocom.uz>
2025-07-22 09:44:21 +0500

    feat: add statistics date tabs

--- src/features/statistics/views/statistics.tsx
+++ src/features/statistics/views/statistics.tsx
@@ -1,8 +1,12 @@
-import useBreadCrumbsStore from '@/store/use-breadcrumbs-store'
 import { useEffect } from 'react'
+import { DatePicker, Tabs } from 'antd'
 import { useTranslation } from 'react-i18next'
-import TouristsByRegionChart from '../containers/tourists-by-region-chart'
+
+import CalendarIcon from '@/components/icons/calendar'
 import FinanceStatsChart from '../containers/finance-stats-chart'
+
+import useBreadCrumbsStore from '@/store/use-breadcrumbs-store'
+import TouristsByRegionChart from '../containers/tourists-by-region-chart'
 import StatisticsList from '../containers/statistics-list'
 import BookingCancelationsTable from '../containers/booking-cancelations-table'
 import IncomeByPaymentType from '../containers/income-by-payment-type'
@@ -23,11 +27,29 @@ const Statistics = () => {
 
   return (
     <div className="p-6 flex flex-col gap-6 flex-1">
-      <div className="flex items-start justify-between">
+      <div className="flex flex-col gap-4">
         <div className="text-[24px] text-primary-dark font-semibold">
           {t('common.statistics')}
         </div>
+        <div className="flex items-center justify-between">
+          <Tabs
+            rootClassName="[&_.ant-tabs-nav]:m-0 [&_.ant-tabs-nav]:w-fit"
+            items={[
+              { label: 'По годам', key: 'year' },
+              { label: 'По месяцам', key: 'month' },
+              { label: 'По недельный', key: 'week' },
+            ]}
+          />
+          <DatePicker
+            size="large"
+            mode="year"
+            suffixIcon={
+              <CalendarIcon className="text-[18px] text-success-dark" />
+            }
+          />
+        </div>
       </div>
+
       <div className="grid grid-cols-4 gap-5">
         <StatisticsList />
         <TouristsByRegionChart className=" col-span-1" />
