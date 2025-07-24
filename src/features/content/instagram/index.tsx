9648ae3da356e0b5077494be1e664efe18438ff1
G'ulomov Alisher <a.g'ulomov@uzinfocom.uz>
2025-07-24 17:26:23 +0500

    feat: create instagram route, ui

--- src/features/content/instagram/index.tsx
+++ src/features/content/instagram/index.tsx
@@ -1,5 +1,31 @@
+import { useState } from 'react'
+import { useNavigate, useParams } from 'react-router'
+import ContentHeader from '../components/content-header'
+import DeleteModal from '../components/delete-modal'
+import InstagramTable from './list/instagram-table'
+
 function InstagramContent() {
-  return <div>Instagram</div>
+  const [deleteOpen, setDeleteOpen] = useState(false)
+  const navigate = useNavigate()
+  const { tab } = useParams()
+
+  return (
+    <div className="flex flex-col gap-4">
+      <ContentHeader
+        title="Instagram"
+        onAddClick={() => {
+          navigate(`/content/${tab}/create`)
+        }}
+      />
+      <InstagramTable setDeleteOpen={setDeleteOpen} />
+      <DeleteModal
+        open={deleteOpen}
+        setOpen={setDeleteOpen}
+        title="Удалить Instagram?"
+        description="Подтвердите, что вы действительно хотите удалить данное направление?"
+      />
+    </div>
+  )
 }
 
 export default InstagramContent
