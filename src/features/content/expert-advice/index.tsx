869a00cfb84349f0a7474878a4156c66ebdfcd57
G'ulomov Alisher <a.g'ulomov@uzinfocom.uz>
2025-07-24 17:26:00 +0500

    feat: create expert-advice route, ui

--- src/features/content/expert-advice/index.tsx
+++ src/features/content/expert-advice/index.tsx
@@ -1,5 +1,31 @@
+import { useState } from 'react'
+import { useNavigate, useParams } from 'react-router'
+import ContentHeader from '../components/content-header'
+import DeleteModal from '../components/delete-modal'
+import TopDestinationsTable from './list/top-destination-table'
+
 function ExpertAdviceContent() {
-  return <div>Expert Advice</div>
+  const [deleteOpen, setDeleteOpen] = useState(false)
+  const navigate = useNavigate()
+  const { tab } = useParams()
+
+  return (
+    <div className="flex flex-col gap-4">
+      <ContentHeader
+        title="Советы экспертов"
+        onAddClick={() => {
+          navigate(`/content/${tab}/create`)
+        }}
+      />
+      <TopDestinationsTable setDeleteOpen={setDeleteOpen} />
+      <DeleteModal
+        open={deleteOpen}
+        setOpen={setDeleteOpen}
+        title="Удалить направление?"
+        description="Подтвердите, что вы действительно хотите удалить данное направление?"
+      />
+    </div>
+  )
 }
 
 export default ExpertAdviceContent
