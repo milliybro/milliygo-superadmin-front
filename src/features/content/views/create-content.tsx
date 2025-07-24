e7d5a3c7ed46181114a7318e4e253435dcdc261d
G'ulomov Alisher <a.g'ulomov@uzinfocom.uz>
2025-07-24 17:36:41 +0500

    refactor: import content routes

--- src/features/content/views/create-content.tsx
+++ src/features/content/views/create-content.tsx
@@ -1,6 +1,9 @@
 import { useLocation } from 'react-router'
 import TopDestinationForm from '../top-destinations/create/top-destination-form'
 import CreateDiscoverContent from '../discover/create/create-discover-content'
+import CreateExpertAdvice from '../expert-advice/create/create-expert-advice'
+import CreateInstagramContent from '../instagram/create/create-instagram-content'
+import CreateEvent from '../events/create/create-events'
 
 const createForms = [
   {
@@ -10,6 +13,18 @@ const createForms = [
   {
     key: 'discover-uzbekistan',
     element: <CreateDiscoverContent />,
+  },
+  {
+    key: 'expert-advice',
+    element: <CreateExpertAdvice />,
+  },
+  {
+    key: 'instagram',
+    element: <CreateInstagramContent />,
+  },
+  {
+    key: 'events',
+    element: <CreateEvent />,
   },
 ]
 
