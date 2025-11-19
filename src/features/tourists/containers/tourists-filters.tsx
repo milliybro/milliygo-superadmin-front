ff1eb05b79a8bc1702ad891ebc9e6ab2bcf77bcd
b_iskandarov <b.iskandarov@uzinfocom.uz>
2025-11-19 14:07:30 +0500

    fix: convert region to number for proper form handling

--- src/features/tourists/containers/tourists-filters.tsx
+++ src/features/tourists/containers/tourists-filters.tsx
@@ -75,7 +75,7 @@ const TouristsFilters = () => {
         full_name: search,
         passport_sn: passport,
         user_information__birth_date: birthday ? dayjs(birthday) : null,
-        user_information__region: region,
+        user_information__region: +region,
         user_information__district: district,
         gender,
       })
