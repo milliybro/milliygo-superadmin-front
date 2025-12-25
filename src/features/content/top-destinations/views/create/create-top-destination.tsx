eb4b9313b3a7c820bb17dba389ebb550e5ddb6d4
Azizbek Raximjonov <azizbek.raximjonov@uzinfocom.uz>
2025-12-25 04:44:47 +0000

    changing react-quill to core quill

--- src/features/content/top-destinations/views/create/create-top-destination.tsx
+++ src/features/content/top-destinations/views/create/create-top-destination.tsx
@@ -167,7 +167,7 @@ export default function TopDestinationForm() {
             {t('content.top_destinations.add-content')}
           </Typography.Title>
           <Divider className="m-0" />
-          <Form.Item name="description" className="mb-0">
+          <Form.Item name="description">
             <QuillEditor />
           </Form.Item>
         </div>
